756dcc6f2f751c7778237e15477ca9e0f5ee2d0e
Sumit Saini <01sumitsaini@gmail.com>
2026-09-25 11:38:28 +0530

    simple express program

--- server.js
+++ server.js
@@ -1,37 +1,45 @@
-const http = require('http');
+const express = require('express');
+const app = express();
 
 let todos = [
-    { id: 1, task: 'hello', completed: false },
-    { id: 2, task: 'world', completed: false },
-    { id: 3, task: '!', completed: false }
+    {
+        id: 1,
+        task: 'hello',
+        completed: false
+    },
+    {
+        id: 2,
+        task: 'world',
+        completed: false
+    },
+    {
+        id: 3,
+        task: '!',
+        completed: false
+    }
 ]
 
-const server = http.createServer((req, res) => {
-    if (req.url === '/todos' && req.method === 'GET') {
-        res.end(JSON.stringify(todos));
-    }
-    else if (req.url === '/todos' && req.method === 'POST') {
-        let body = '';
-        req.on('data', chunk => {
-            body += chunk.toString();
-        });
-        req.on('end', () => {
-            todos.push(JSON.parse(body));
-            res.end(JSON.stringify(todos));
-        });
-    }
-    else if (req.url === '/todos' && req.method === 'DELETE') {
-        let body = '';
-        req.on('data', chunk => {
-            body += chunk.toString();
-        });
-        req.on('end', () => {
-            todos = todos.filter(todo => todo.id !== JSON.parse(body).id);
-            res.end(JSON.stringify(todos));
-        });
-    }
-})
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
 
-server.listen(3000, () => {
-    console.log('listening on port 3000');
+app.get('/todos', (req, res) => {
+    res.status(200).json(todos);
+});
+
+app.post('/todos', (req, res) => {
+    let body = req.body;
+
+    todos.push(JSON.parse(body));
+    res.status(200).json(todos);
+});
+
+app.delete('/todos', (req, res) => {
+    let body = req.body;
+    todos = todos.filter(todo => todo.id !== JSON.parse(body).id);
+    res.status(200).json(todos);
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
 });
